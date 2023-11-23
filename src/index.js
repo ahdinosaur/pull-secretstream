d0eb461fb4cc7ac157c637420b847126c6684eec
Michael Williams <michael.williams@enspiral.com>
2023-11-23 13:20:44 +1300

    add debugging, also change how header is sent to aid debugging

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const { KEYBYTES, HEADERBYTES, Push, Pull } = require('sodium-secretstream')
 const pull = require('pull-stream/pull')
 const pullCat = require('pull-cat')
-const pullMapLast = require('pull-map-last')
 const pullHeader = require('pull-header')
 const pullThrough = require('pull-through')
+const createDebug = require('debug')
+
+createDebug.formatters.h = (v) => {
+  return v.toString('hex')
+}
+
+const debug = createDebug('pull-secretstream')
 
 module.exports = {
   KEYBYTES,
@@ -15,22 +21,42 @@ function createEncryptStream(key) {
   if (key.length !== KEYBYTES) {
     throw new Error(`pull-secretstream/createEncryptStream: key must be byte length of ${KEYBYTES}`)
   }
+  const debugKey = key.slice(0, 2)
 
   const encrypter = new Push(key)
 
-  const sendHeader = pull.values([encrypter.header])
+  const sendHeader = () => {
+    let hasSentHeader = false
+    return (end, cb) => {
+      if (end) cb(end)
+      else if (!hasSentHeader) {
+        const header = encrypter.header
+        debug('%h : encrypter sending header %h', debugKey, header)
+        hasSentHeader = true
+        cb(null, header)
+      } else {
+        cb(true)
+      }
+    }
+  }
 
-  const encryptMap = pullMapLast(
-    (plaintext) => {
-      return encrypter.next(plaintext)
+  const encryptMap = pullThrough(
+    function encryptThroughData(plaintext) {
+      debug('%h : encrypting plaintext %h', debugKey, plaintext)
+      const ciphertext = encrypter.next(plaintext)
+      debug('%h : encrypted ciphertext %h', debugKey, ciphertext)
+      this.queue(ciphertext)
     },
-    () => {
-      return encrypter.final()
+    function encryptThroughEnd() {
+      const final = encrypter.final()
+      debug('%h : encrypter final %h', debugKey, final)
+      this.queue(final)
+      this.queue(null)
     },
   )
 
   return (stream) => {
-    return pullCat([sendHeader, pull(stream, encryptMap)])
+    return pullCat([sendHeader(), pull(stream, encryptMap)])
   }
 }
 
@@ -38,17 +64,23 @@ function createDecryptStream(key) {
   if (key.length !== KEYBYTES) {
     throw new Error(`pull-secretstream/createDecryptStream: key must be byte length of ${KEYBYTES}`)
   }
+  const debugKey = key.slice(0, 2)
 
   const decrypter = new Pull(key)
 
   const receiveHeader = pullHeader(HEADERBYTES, (header) => {
+    debug('%h : decrypter receiving header %h', debugKey, header)
     decrypter.init(header)
   })
 
   const decryptMap = pullThrough(
     function decryptThroughData(ciphertext) {
-      this.queue(decrypter.next(ciphertext))
+      debug('%h : decrypting ciphertext %h', debugKey, ciphertext)
+      const plaintext = decrypter.next(ciphertext)
+      debug('%h : decrypted ciphertext %h', debugKey, plaintext)
+      this.queue(plaintext)
       if (decrypter.final) {
+        debug('%h : decrypter final', debugKey)
         this.emit('end')
       }
     },
