2a22343b388fd27fdbabac2a081fa14be0440652
Michael Williams <michael.williams@enspiral.com>
2023-11-24 13:17:27 +1300

    batch buffers within microtask (and thus handle the final marker more efficiently)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const { KEYBYTES, HEADERBYTES, ABYTES, Push, Pull } = require('sodium-secretstream')
 const { BufferList } = require('bl')
 const pull = require('pull-stream/pull')
+const pullEmpty = require('pull-stream/sources/empty')
+const pullOnce = require('pull-stream/sources/once')
+const pullError = require('pull-stream/sources/error')
+const pullFlatten = require('pull-stream/throughs/flatten')
+const pullMapLast = require('pull-map-last')
 const pullCat = require('pull-cat')
 const pullHeader = require('pull-header')
+const pullPushable = require('pull-pushable')
 const pullThrough = require('pull-through')
 const createDebug = require('debug')
 const { sodium_pad, sodium_unpad } = require('sodium-universal')
+const b4a = require('b4a')
 
 const DEFAULT_BLOCK_SIZE = 512 // bytes
 
@@ -52,46 +59,101 @@ function createEncryptStream(key, ciphertextBlockSize = DEFAULT_BLOCK_SIZE) {
     }
   }
 
-  const encryptMap = pullThrough(
-    function encryptThroughData(plaintext) {
-      plaintextBufferList.append(plaintext)
+  const encryptMap = () => {
+    let hasQueuedMicrotask = false
+    let hasEnded = false
 
-      // while we still have enough bytes to send full blocks
-      while (plaintextBufferList.length >= plaintextBlockSize) {
-        const plaintextBlock = plaintextBufferList.slice(0, plaintextBlockSize)
-        plaintextBufferList.consume(plaintextBlockSize)
-        debug('%h : encrypting block %h', debugKey, plaintextBlock)
-        const ciphertext = encrypter.next(plaintextBlock)
-        debug('%h : encrypted ciphertext %h', debugKey, ciphertext)
-        this.queue(ciphertext)
-      }
+    return pull(
+      pullMapLast(
+        function encryptMapData(plaintext) {
+          plaintextBufferList.append(plaintext)
 
-      // send the remaining as a padded block
-      if (plaintextBufferList.length > 0) {
-        const plaintextLength = plaintextBufferList.length
-        const plaintextBlock = Buffer.alloc(plaintextBlockSize)
-        plaintextBufferList.copy(plaintextBlock, 0, 0, plaintextLength)
-        plaintextBufferList.consume(plaintextLength)
-        sodium_pad(plaintextBlock, plaintextLength, plaintextBlockSize)
-        debug('%h : encrypting padded block %h', debugKey, plaintextBlock)
-        const ciphertext = encrypter.next(plaintextBlock)
-        debug('%h : encrypted ciphertext %h', debugKey, ciphertext)
-        this.queue(ciphertext)
-      }
-    },
-    function encryptThroughEnd() {
-      // send a block full of zeros with the final marker
-      const finalBlock = Buffer.alloc(plaintextBlockSize)
-      sodium_pad(finalBlock, 0, plaintextBlockSize)
-      const final = encrypter.final(finalBlock, Buffer.allocUnsafe(plaintextBlockSize + ABYTES))
-      debug('%h : encrypter final %h', debugKey, final)
-      this.queue(final)
-      this.queue(null)
-    },
-  )
+          // for each event loop, run one microtask that will
+          //   encrypt and send all of our queued buffers.
+          //   (including the final marker if we ended.)
+          if (!hasQueuedMicrotask) {
+            debug('%h : queued microtask', debugKey)
+            hasQueuedMicrotask = true
+            const pushable = pullPushable(true)
+            queueMicrotask(() => {
+              debug('%h : running microtask', debugKey)
+              hasQueuedMicrotask = false
+              try {
+                pushCiphertexts(pushable.push, hasEnded)
+                pushable.end()
+              } catch (err) {
+                pushable.end(err)
+              }
+            })
+            return pushable.source
+          }
+
+          return pullEmpty()
+        },
+        function encryptMapEnd() {
+          debug('%h : ending encrypt map', debugKey)
+
+          hasEnded = true
+
+          // if we have a microtask, that will send the final marker.
+          if (!hasQueuedMicrotask) {
+            try {
+              const final = getFinal()
+              return pullOnce(final)
+            } catch (err) {
+              return pullError(err)
+            }
+          }
+
+          return pullEmpty()
+        },
+      ),
+      pullFlatten(),
+    )
+  }
 
   return (stream) => {
-    return pullCat([sendHeader(), pull(stream, encryptMap)])
+    return pullCat([sendHeader(), pull(stream, encryptMap())])
+  }
+
+  function pushCiphertexts(push, hasEnded) {
+    // while we still have enough bytes to send full blocks
+    while (plaintextBufferList.length >= plaintextBlockSize) {
+      const plaintextBlock = plaintextBufferList.slice(0, plaintextBlockSize)
+      plaintextBufferList.consume(plaintextBlockSize)
+      debug('%h : encrypting block %h', debugKey, plaintextBlock)
+      const ciphertext = encrypter.next(plaintextBlock)
+      debug('%h : encrypted ciphertext %h', debugKey, ciphertext)
+      push(ciphertext)
+    }
+
+    // send the remaining as a padded block
+    if (plaintextBufferList.length > 0) {
+      const plaintextLength = plaintextBufferList.length
+      const plaintextBlock = Buffer.alloc(plaintextBlockSize)
+      plaintextBufferList.copy(plaintextBlock, 0, 0, plaintextLength)
+      plaintextBufferList.consume(plaintextLength)
+      sodium_pad(plaintextBlock, plaintextLength, plaintextBlockSize)
+      debug('%h : encrypting padded block %h', debugKey, plaintextBlock)
+      let ciphertext
+      if (!hasEnded) {
+        ciphertext = encrypter.next(plaintextBlock)
+        debug('%h : encrypted ciphertext %h', debugKey, ciphertext)
+      } else {
+        ciphertext = encrypter.final(plaintextBlock, b4a.allocUnsafe(plaintextBlockSize + ABYTES))
+        debug('%h : encrypted final %h', debugKey, ciphertext)
+      }
+      push(ciphertext)
+    }
+  }
+
+  function getFinal() {
+    // send a block full of zeros with the final marker
+    const finalBlock = Buffer.alloc(plaintextBlockSize)
+    sodium_pad(finalBlock, 0, plaintextBlockSize)
+    const final = encrypter.final(finalBlock, b4a.allocUnsafe(plaintextBlockSize + ABYTES))
+    debug('%h : encrypted final %h', debugKey, final)
+    return final
   }
 }
 
